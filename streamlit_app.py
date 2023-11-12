bd72f02556d34502deefdb672e506f7e2a654734
onana001 <150483553+onana001@users.noreply.github.com>
2023-11-12 11:21:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,5 @@ import streamlit
 
 streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-stramlit.text('🥑🍞 Avacado Toast') 
+ 
                
