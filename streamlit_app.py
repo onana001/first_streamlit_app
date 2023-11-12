3ddf31a7d72bac0e250822fdea21e9dbc9f229b5
onana001 <150483553+onana001@users.noreply.github.com>
2023-11-12 10:43:10 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
+
 Streamlit.text('Omega 3 & Blueberry Oatmeal')
 Streamlit.text('Kale, Spinach & Rocket Smothie')
 Streamlit.text('Hard-Boiled Free-Range Egg') 
